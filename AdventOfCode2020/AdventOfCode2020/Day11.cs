839f13d8c244da450427e485db6bca0c0df14e31
Vilhelm Flach <vilhelm.flach@cybercom.com>
2020-12-05 22:11:24 +0100

    Prepare days 06-12

---
+++ AdventOfCode2020/AdventOfCode2020/Day11.cs
@@ -0,0 +1,18 @@
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AdventOfCode2020
+{
+    public class Day11 : Day
+    {
+        public override string SolvePartOne()
+        {
+            return "";
+        }
+        
+        public override string SolvePartTwo()
+        {
+            return "";
+        }
+    }
+}
