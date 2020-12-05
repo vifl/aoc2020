127e145bbf31e136cddfbdba941b5e9fde1fe24c
Vilhelm Flach <vilhelm.flach@cybercom.com>
2020-12-05 22:56:26 +0100

    Null check

--- AdventOfCode2020/AdventOfCode2020/Program.cs
+++ AdventOfCode2020/AdventOfCode2020/Program.cs
@@ -9,6 +9,11 @@ namespace AdventOfCode2020
         {
             var typeName = "AdventOfCode2020.Day" + DateTime.Now.Day.ToString("00");
             var day = (Day) Activator.CreateInstance("AdventOfCode2020", typeName).Unwrap();
+            if (day == null)
+            {
+                Console.WriteLine("No implementation available for today");
+                return;
+            }
 
             Console.WriteLine($"Day {day.DayNumber} part one: {day.SolvePartOne()}");
             Console.WriteLine($"Day {day.DayNumber} part two: {day.SolvePartTwo()}");
