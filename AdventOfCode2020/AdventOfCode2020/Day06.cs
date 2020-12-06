a9377dbaac09d64ab5dee574c6e9cfd40e5130e5
Vilhelm Flach <vilhelm.flach@cybercom.com>
2020-12-06 12:38:05 +0100

    Day 6 solution

--- AdventOfCode2020/AdventOfCode2020/Day06.cs
+++ AdventOfCode2020/AdventOfCode2020/Day06.cs
@@ -1,18 +1,38 @@
-using System.Collections.Generic;
+using System;
+using System.IO;
 using System.Linq;
 
 namespace AdventOfCode2020
 {
     public class Day06 : Day
     {
+        private const string Letters = "abcdefghijklmnopqrstuvwxyz";
+
         public override string SolvePartOne()
         {
-            return "";
+            var formData = GetFormData();
+            var count = formData.Select(groupFormData => groupFormData.ToCharArray().Intersect(Letters).Distinct().Count()).Sum();
+            return "" + count;
+        }
+
+        private string[] GetFormData()
+        {
+            var allText = File.ReadAllText($"input{DayNumber}.txt");
+            return allText.Split(new[] {"\r\n\r\n"}, StringSplitOptions.RemoveEmptyEntries);
         }
         
         public override string SolvePartTwo()
         {
-            return "";
+            var formData = GetFormData();
+            var count = 0;
+            
+            foreach (var groupFormData in formData)
+            {
+                var expectedCount = groupFormData.Split(new []{"\r\n"}, StringSplitOptions.None).Length;
+                var allMatching = groupFormData.ToCharArray().Select(c => c).Where(c => Letters.Contains(c)).GroupBy(c => c).Where(g => g.Count() == expectedCount);
+                count += allMatching.Count();
+            } 
+            return "" + count;
         }
     }
 }
