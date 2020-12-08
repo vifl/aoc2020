874234a8c1ec1e6c9f0615f3dfb369aede972d92
vifl <vilhelm.flach@cybercom.com>
2020-12-08 12:53:49 +0100

    Program.cs improvements

--- AdventOfCode2020/AdventOfCode2020/Program.cs
+++ AdventOfCode2020/AdventOfCode2020/Program.cs
@@ -10,7 +10,13 @@ namespace AdventOfCode2020
     {
         public static void Main(string[] args)
         {
-            var className = CreateTodaysClassIfNeeded();
+            var created = CreateTodaysClassIfNeeded();
+            if (created)
+            {
+                return;
+            }
+
+            var className = GetClassName();
             var typeName = "AdventOfCode2020." + className;
             
             var day = (Day) Activator.CreateInstance("AdventOfCode2020", typeName).Unwrap();
@@ -24,49 +30,58 @@ namespace AdventOfCode2020
             Console.WriteLine($"Day {day.DayNumber} part two: {day.SolvePartTwo()}");
         }
 
-        private static string CreateTodaysClassIfNeeded()
+        private static string GetClassName()
+        {
+            return $"Day{DateTime.Now.Day:00}";
+        }
+
+        private static bool CreateTodaysClassIfNeeded()
         {
             var day = DateTime.Now.Day;
             var year = DateTime.Now.Year;
-            var className = $"Day{day:00}";
+            var className = GetClassName();
             var projectDirectory = GetBaseDirectory();
-            var classFilename = $"y{className}.cs";
+            var classFilename = $"{className}.cs";
             var classFileFullPath = Path.Combine(projectDirectory, classFilename);
-            var puzzleFilename = $"yinput{day:00}.txt";
+            var puzzleFilename = $"input{day:00}.txt";
             var puzzleFileFullPath = Path.Combine(projectDirectory, puzzleFilename);
 
-            if (!File.Exists(classFileFullPath))
+            if (File.Exists(classFileFullPath))
             {
-                Console.WriteLine($"{classFilename} not found, will create.");
-                var classContempt = DayTemplate.Replace("#day#", $"{day:00}");
-                File.WriteAllText(classFileFullPath, classContempt);
-
-                Console.Write($"Fetching puzzle input...");
-                try
-                {
-                    var puzzleContent = GetPuzzleInput(year, day);
-                    Console.WriteLine($" done.");
-                    File.WriteAllText(puzzleFileFullPath, puzzleContent.Trim());
-                    Console.WriteLine($"Created {puzzleFilename}");
-                }
-                catch (Exception e)
-                {
-                    Console.WriteLine($" failed: {e.Message}");
-                    Console.WriteLine($"Created empty {puzzleFilename}");
-                    File.WriteAllText(puzzleFileFullPath, "");
-                }
-
-                var projectFile = Path.Combine(projectDirectory, "AdventOfCode2020.csproj"); 
-                
-                var p = new Microsoft.Build.Evaluation.Project(projectFile);
-                p.AddItem("Compile", classFilename);
-                var kvp = new KeyValuePair<string, string>("CopyToOutputDirectory", "PreserveNewest");
-                
-                p.AddItem("Content", puzzleFilename, new [] {kvp});
-                p.Save();
+                return false;
             }
-           
-            return className;
+
+        Console.WriteLine($"{classFilename} not found, will create.");
+            var classContempt = DayTemplate.Replace("#day#", $"{day:00}");
+            File.WriteAllText(classFileFullPath, classContempt);
+
+            Console.Write($"Fetching puzzle input...");
+            try
+            {
+                var puzzleContent = GetPuzzleInput(year, day);
+                Console.WriteLine($" done.");
+                File.WriteAllText(puzzleFileFullPath, puzzleContent.Trim());
+                Console.WriteLine($"Created {puzzleFilename}");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($" failed: {e.Message}");
+                Console.WriteLine($"Created empty {puzzleFilename}");
+                File.WriteAllText(puzzleFileFullPath, "");
+            }
+
+            var projectFile = Path.Combine(projectDirectory, "AdventOfCode2020.csproj"); 
+            
+            var p = new Microsoft.Build.Evaluation.Project(projectFile);
+            p.AddItem("Compile", classFilename);
+            var kvp = new KeyValuePair<string, string>("CopyToOutputDirectory", "PreserveNewest");
+            
+            p.AddItem("Content", puzzleFilename, new [] {kvp});
+            p.Save();
+
+            Console.WriteLine($"Class and data files created. Execution halted. Please edit {classFilename} and then run again.");
+
+            return true;
         }
         
         private static string GetBaseDirectory()
