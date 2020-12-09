150f26a69e8bcc08f9308e6ec9b02b330c914d68
vifl <vilhelm.flach@cybercom.com>
2020-12-09 20:57:24 +0100

    Day 9 solution

---
+++ AdventOfCode2020/AdventOfCode2020/Day09.cs
@@ -0,0 +1,101 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace AdventOfCode2020
+{
+    public class Day09 : Day
+    {
+        public override string SolvePartOne()
+        {
+             // var numbers = testdata.Split("\r\n".ToCharArray(), StringSplitOptions.RemoveEmptyEntries).Select(line => long.Parse(line)).ToList();
+             // return "" + FindWeakness(numbers, 5);
+            var numbers = GetPuzzleInput().Select(line => long.Parse(line)).ToList();
+            return "" + FindWeakness(numbers, 25);
+        }
+
+        private long FindWeakness(List<long> numbers, int preambleLength)
+        {
+            for (var i = preambleLength; i < numbers.Count; i++)
+            {
+                if (!IsSumOfTwoInSeries(numbers[i], numbers.GetRange(i - preambleLength, preambleLength)))
+                {
+                    return numbers[i];
+                }
+            }
+
+            throw new Exception("failed to find solution");
+        }
+
+        private bool IsSumOfTwoInSeries(long expectedNumber, List<long> range)
+        {
+            for (var i = 0; i < range.Count(); i++)
+            {
+                var term1 = range[i];
+                for (var j = 0; j < range.Count(); j++)
+                {
+                    var term2 = range[j];
+
+                    if (term1 == term2)
+                        continue;
+                    if (term1 + term2 == expectedNumber)
+                        return true;
+                }
+                
+            }
+            return false;
+        }
+
+        public override string SolvePartTwo()
+        {
+            // var numbers = testdata.Split("\r\n".ToCharArray(), StringSplitOptions.RemoveEmptyEntries).Select(line => long.Parse(line)).ToList();
+            // var weakness = FindWeakness(numbers, 5);
+            var numbers = GetPuzzleInput().Select(line => long.Parse(line)).ToList();
+            var weakness = FindWeakness(numbers, 25);
+
+             long? result = null;
+             for (var start = 0; start < numbers.Count && result == null; start++)
+             {
+                 var sum = 0L;
+                 for (var i = start; i < numbers.Count(); i++)
+                 {
+                     sum += numbers[i];
+                     if (sum == weakness)
+                     {
+                         var range = numbers.GetRange(start, i - start);
+                         
+                         // match
+                         result = range.Min() + range.Max();
+                             
+                         break;
+                     }
+                 }
+             }
+
+             return "" + result.Value;
+        }
+        
+        private string testdata = @"35
+20
+15
+25
+47
+40
+62
+55
+65
+95
+102
+117
+150
+182
+127
+219
+299
+277
+309
+576";
+
+    }
+}
